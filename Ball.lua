0299f849c8a966f1c2b0ff17366c3b729ca5e96d
yurilpbh <yurilpbh@hotmail.com>
2020-07-09 15:04:44 -0300

    Added more specification about the functions

--- Ball.lua
+++ Ball.lua
@@ -1,15 +1,17 @@
 Ball = Class{}
 
+--Initialize a ball with position (x,y) and size (width,height)
 function Ball:init(x, y, width, height)
     self.x = x
     self.y = y
     self.width = width
     self.height = height
 
-    self.dx = math.random(2) == 1 and -100 or 100
+    self.dx = 0
     self.dy = math.random(-50, 50)
 end
 
+--Verify a collision between the ball and a box
 function Ball:collides(box)
     if  self.x > box.x + box.width or self.x + self.width < box.x then
         return false
@@ -20,23 +22,23 @@ function Ball:collides(box)
     return true
 end
 
+--Reset the ball to the initial position
 function Ball:reset()
     --Start ball's position in the middle of the screen
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
 
-    --Given ball's x and y velocity a random starting value
-    --the and/or pattern here is Lua's way of accomplishing a ternary operation
-    --in other programming language like C
-    self.dx = math.random(2) == 1 and -100 or 100
+    --Given ball's y velocity a random starting value
     self.dy = math.random(-50, 50)
 end
 
+--Updates the ball position
 function Ball:update(dt)
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
 end
 
+----Render the ball, make it visible on the screen
 function Ball:render()
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
