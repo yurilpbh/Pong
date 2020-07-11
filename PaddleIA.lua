9b385faead2c9774c1c795a672746186c862bd1d
yurilpbh <yurilpbh@hotmail.com>
2020-07-11 17:33:08 -0300

    Added functions to save the progress of learning

--- PaddleIA.lua
+++ PaddleIA.lua
@@ -1,16 +1,17 @@
 PaddleIA = Class{}
 
-local w11, w21, w31, w41, w51, w61, w12, w22, w32, w42, w52, w62 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-local learningRate = 0.1
+local w11, w21, w31, w41, w51, w61, w12, w22, w32, w42, w52, w62 = 505.76, 69780.36, -186.78, -1709.64, 210.63, -69244.43, 226.82, -54670.46, 276.34, 5907.17, 83.54, 56708.28
+local learningRate = 0.05
 
 --Initialize a paddle with position (x,y) and size (width,height)
-function PaddleIA:init(x, y, width, height, paddle_speed)
+function PaddleIA:init(x, y, width, height, paddle_speed, percent)
     self.x = x
     self.y = y
     self.width = width
     self.height = height
     self.PADDLE_SPEED = paddle_speed
     self.dy = 0
+    w11, w21, w31, w41, w51, w61, w12, w22, w32, w42, w52, w62 =  w11 * percent, w21 * percent, w31 * percent, w41 * percent, w51 * percent, w61 * percent, w12 * percent, w22 * percent, w32 * percent, w42 * percent, w52 * percent, w62 * percent
 end
 
 --Update the position of the paddle
@@ -65,4 +66,23 @@ function updateW(ball, paddle)
     w42 = w42 + learningRate * (targeto2 - o2) * ball.dy
     w52 = w52 + learningRate * (targeto2 - o2) * paddle.x
     w62 = w62 + learningRate * (targeto2 - o2) * paddle.y
+end
+
+function PaddleIA:save(filePath)
+    -- Opens a file in read
+    file = io.open(filePath, "a+")
+
+    -- sets the default output file as test.lua
+    io.output(file)
+
+    -- appends a word test to the last line of the file
+    io.write(string.format("%.2f", w11) .. ", " .. string.format("%.2f", w21) .. ", " .. string.format("%.2f", w31) .. ", " .. string.format("%.2f", w41) .. ", " .. string.format("%.2f", w51) .. ", " .. string.format("%.2f", w61) .. "\n")
+    io.write(string.format("%.2f", w12) .. ", " .. string.format("%.2f", w22) .. ", " .. string.format("%.2f", w32) .. ", " .. string.format("%.2f", w42) .. ", " .. string.format("%.2f", w52) .. ", " .. string.format("%.2f", w62) .. "\n" .. "\n")
+
+    -- closes the open file
+    io.close(file)
+end
+
+function PaddleIA:changeLR(newLR)
+    learningRate = newLR
 end
